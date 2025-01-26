0d97065b40a4807429df3167bc7176577c760cec
takoyaki10 <takukato224@gmail.com>
2025-01-26 22:30:36 +0900

    Update script.js

--- script.js
+++ script.js
@@ -3,16 +3,16 @@ const questions = [
         question: "Q1 アイクラと言えばポポと？",
         choices: ["バカ", "チャチャ", "ボポボ", "ナナ"],
         correct: 3,
-        explanation: "アイクラといえばポポとナナ 2人は友達以上恋人未満の関係だがスマブラDXでは恋愛関係がほのめかされている"
+        explanation: "アイクラといえばポポとナナ 2人は友達以上恋人未満の関係だがスマブラDXでは恋愛関係がほのめかされているよ"
     },
     {
-        question: "Q2 最もセンスのいいカラーは？",
+        question: "Q2 最もセンスのいいカラーは？<br>※作問者の独断とする",
         choices: ["1P(<span style='color: rgb(106, 90, 205);'>あお</span>/<span style='color: pink;'>ピンク</span>)", "2P(<span style='color: blue;'>あお</span>/<span style='color: rgb(0, 132, 255);'>みずいろ</span>)", "7P(<span style='color: lightcyan;'>みずいろ</span>/<span style='color: lightcyan;'>みずいろ</span>)", "8(<span style='color: orange;'>オレンジ</span>/<span style='color: purple;'>むらさき</span>)"],
         correct: 1,
-        explanation: "問題の枠組みのデザインが青と水色を基調としていることから制作者にとってセンスのいいカラーは2Pだとわかる 忖度ではない"
+        explanation: "問題の枠組みのデザインが青と水色を基調としていることから作問者にとってセンスのいいカラーは2Pだと推測できるよね 忖度ではない"
     },
     {
-        question: "Q3 次の選択肢のうち, あゆじゃん切り離し下投げ相方空前<br>を当てた際にファルコが立ち吹っ飛びになる最大の蓄積ダメージは？<br><br><b>条件 スマメイトルール1on1 アイクラ側はどちらも0% OPは初期状態<br>相方は大ジャンプ空前 回答は下投げ直前の%</b>",
+        question: "Q3 次の選択肢のうち, あゆじゃん切り離し下投げ相方空前を当てた際にファルコが立ち吹っ飛びになる最大の蓄積ダメージは？<br><br><b>条件 スマメイトルール1on1 アイクラ側はどちらも0% OPは初期状態 相方は大ジャンプ空前 回答は下投げ直前の%</b>",
         choices: ["17%", "18%", "19%", "20%"],
         correct: 0,
         explanation: "ファルコは軽い 前投げ空前は掴み打撃2回いけるけど下投げ空前は間に合わないので別解はないよ"
@@ -21,7 +21,7 @@ const questions = [
         question: "Q4 アイクラの参戦ナンバーは？",
         choices: ["14", "15", "16", "17"],
         correct: 1,
-        explanation: "選択画面では17番目だがダークサムスとデイジーがダッシュファイターのため15番目"
+        explanation: "選択画面では17番目だがダークサムスとデイジーがダッシュファイターだから15番だよ"
     },
     {
         question: "Q5 アイクラが負けた時にリザルトでする拍手は",
@@ -42,10 +42,10 @@ const questions = [
         explanation: "ナナを助けてもポポが消えたタイミングでナナも消えてしまうためポポを助けよう！沈黙しちゃだめだよ"
     },
     {
-        question: "Q8 スマブラSPにおいてアイクラの横強がアプデで変更されポポナックル&ナナナックル(通称ポックルナックル)になりました 1Pカラーの場合ポポが殴った後に同じ動きでナナが殴る技です 2人の攻撃がヒットした際の全体ダメージが70%のときポポの攻撃のダメージは何%でしょうか",
+        question: "Q8 スマブラSPにおいてアイクラの横強がアプデで変更されポポナックル&ナナナックル(通称ポックルナックル)になりました 1Pカラーの場合ポポが殴った後に同じ動きでナナが殴る技です 2人の攻撃がヒットした際の全体ダメージが70%のときポポの攻撃のダメージは何%？",
         choices: ["35%", "40%", "42%", "45%"],
         correct: 1,
-        explanation: "相方の技のダメージは本体の0.75倍なので後は計算を頑張ってね NBとかは例外だよ"
+        explanation: "相方の技のダメージは本体の0.75倍に設定されている 後は計算を頑張ってね NBとかは例外だよ"
     },
     {
         question: "Q9 ポポとナナが本気でかけっこしたら勝つのはどっち？<br>ただしスマブラSPにおける走行速度の最大値を本気と定義する",
@@ -54,10 +54,10 @@ const questions = [
         explanation: "本体と相方では相方の方が合流時などの走行速度が高く設定されている ただしカラバリによってポポとナナは入れ替わるので最大値は同じ"
     },
     {
-        question: "Q10 終点の両端にアイクラとファルコが向かい合いアイクラがNB(アイスショット)を使用します 地上でNBをした時と小ジャンプの頂点でNBをした時のファルコに与えるダメージについての記述で正しいのは？",
+        question: "Q10(ラスト) 終点の両端にアイクラとファルコが向かい合いアイクラがNBをします 地上でNBをした時と小ジャンプの頂点でNBをした時のファルコに与えるダメージについての記述で正しいのは？",
         choices: ["ちじょう > しょうジャンプ", "ちじょう < しょうジャンプ", "おなじ", "ランダム"],
         correct: 1,
-        explanation: "NBは一定距離滑った後は地上を滑るほど大きさが小さくなりダメージも減ってしまう ジャンプNBだと地上を滑る距離が短くなるのでダメージが高いのだ"
+        explanation: "NBは地上を滑るほど大きさが小さくなりダメージも減ってしまうよ(最初の数fは減らない) ジャンプNBだと地上を滑る距離が短くなるのでダメージが高いのだ"
     }
 ];
 
@@ -128,10 +128,10 @@ function showResult() {
     if(newscore == 80) document.getElementById("last-text").innerText = `ゆうとうせい ポポなみのちのう`;
     if(newscore == 70) document.getElementById("last-text").innerText = `わるないね ナナくらいのかしこさ`;
     if(newscore == 60) document.getElementById("last-text").innerText = `ふつうのじんせい それもよし`;
-    if(newscore == 50) document.getElementById("last-text").innerText = `いうことがない ホームランならすごい`;
+    if(newscore == 50) document.getElementById("last-text").innerText = `とくにいうことがない ホームランならすごい`;
     if(newscore == 40) document.getElementById("last-text").innerText = `よくないよ アイクラもないている`;
     if(newscore == 30) document.getElementById("last-text").innerText = `すごい てきとうにえらんだみたいなてんすう`;
-    if(newscore == 20) document.getElementById("last-text").innerText = `だりつでもひくい めつぶってもおまえにかてる`;
+    if(newscore == 20) document.getElementById("last-text").innerText = `ひかんしちゃだめ やっぱしろ`;
     if(newscore == 10) document.getElementById("last-text").innerText = `よろこべ 100てんまんのテストだよ`;
     if(newscore == 0) document.getElementById("last-text").innerText = `ぎゃくにすごい わけでもない`;
     const container = document.getElementById("normal-container");
